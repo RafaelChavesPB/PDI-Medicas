5db2f8de04e7dc7750b4c3b1450acd253bba707e
igor-stefan <igorstefan@aol.com>
2023-03-13 12:53:17 -0300

    sharpened repo and made code for atvd4

--- Stefan/atvd4/main.cpp
+++ Stefan/atvd4/main.cpp
@@ -12,22 +12,83 @@ int main(int argc, char** argv){
         // Gets filename
         std::string imgName = argv[i + 1];
         clearString(imgName);
+        
+        printf("\nProcessing \"%s\" image...\n", imgName.c_str());
 
         // Creates folder
-        std::string folderName = "ResultsIMG[" + imgName + "]";
+        std::string folderName = "Results-IMG[" + imgName + "]";
         createFolder(folderName.c_str());
 
-        // Folder for gray images
-        std::string path = folderName + "/GRAY";
-        createFolder(path.c_str());
+        // Sets path variabel for future work
+        std::string path = "";
+
+        /////////////////////////// Gray Images //////////////////////////////////
+
+        // Gray Image by mean and by weight
+        cv::Mat imgGrayMean = cv::Mat::zeros(image.size(), CV_8UC1);
+        cv::Mat imgGrayWeight = cv::Mat::zeros(image.size(), CV_8UC1);
 
         for(int i = 0; i < image.rows; i++) {
             for(int j = 0; j < image.cols; j++) {
                 cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
-                // Faça algo com o pixel
+                imgGrayMean.at<uchar>(i, j) = getGrayValueFromBGRPixel(pixel, true);
+                imgGrayWeight.at<uchar>(i, j) = getGrayValueFromBGRPixel(pixel, false);
             }
         }
-        cv::imwrite("hohoho.png", image);
+        
+        printf("Spatial Info for %s = %.2f\n", imgName.c_str(), calculateSpatialInfo(imgGrayWeight));
+
+         // Folder for writing gray image results
+        path = folderName + "/GRAY";
+        createFolder(path.c_str());
+        
+        cv::imwrite(path + "/mean.png", imgGrayMean);
+        cv::imwrite(path + "/weight.png", imgGrayWeight);
+        
+        ////////////////////////////// HSV Images //////////////////////////////////
+        // Converting image
+        cv::Mat hsv_img;
+        cv::cvtColor(image, hsv_img, cv::COLOR_BGR2HSV);
+        
+        // int maxval = -1;
+        // for(int i = 0; i < hsv_img.rows; i++){
+        //     for(int j = 0; j < hsv_img.cols; j++){
+        //         cv::Vec3b pixel = hsv_img.at<cv::Vec3b>(i, j);
+        //         printf("[%d, %d, %d] ", pixel[0], pixel[1], pixel[2]);
+        //         if(pixel[0] > maxval) maxval = pixel[0];
+        //     }
+        //     printf("\n");
+        // }
+        // printf("Max val = %d\n", maxval);
+
+        // Folder for hsv images
+        path = folderName + "/HSV";
+        createFolder(path.c_str());
+
+        cv::imwrite(path + "/ORIG_hsv.png", hsv_img);
+
+        ///////////////////////////// Color Images //////////////////////////////////
+        // Folder for color processing
+        path = folderName + "/COLOR";
+        createFolder(path.c_str());
+        std::string cp_path = "";
+        int step = 15;
+        int maxlim = step * 12 + 1;
+
+        for(int lim = step, idx = 0; lim < maxlim; lim += step, idx++){
+            cv::Mat mask, result;
+            cv::Scalar lower = cv::Scalar(lim - step, 0, 0);
+            cv::Scalar upper = cv::Scalar(lim - 1, 255, 255);
+
+            cv::inRange(hsv_img, lower, upper, mask);
+            cv::bitwise_and(image, image, result, mask);
+
+            cp_path = path + "/" + colorNames[idx];
+            createFolder(cp_path.c_str());
+            cv::imwrite(cp_path + "/mask.png", mask);
+            cv::imwrite(cp_path + "/result.png", result);
+        }
+
     }
     return 0;
 }
