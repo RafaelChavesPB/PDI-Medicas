fa2202bd858c25f8703bb3722c97cf93bb11f0ca
Rafael Cavalcante chaves <rafael.chaves@polodeinovacao.ifpb.edu.br>
2023-04-22 14:50:32 -0300

    Ball 1

--- Rafael/atividade7/main.cpp
+++ Rafael/atividade7/main.cpp
@@ -3,29 +3,6 @@
 
 using namespace cv;
 
-void getImageLayers(Mat &src, Mat *dst);
-void getMaskFromHue(Mat &src, Mat &dst, int begin, int end);
-
-int main()
-{
-    char filename[100];
-
-    Mat gray, bgr, hsv;
-    bgr = imread("../images/ball.jpg", IMREAD_COLOR);
-    GaussianBlur(bgr, bgr, Size(3, 3), 0, 0, BORDER_DEFAULT);
-    cvtColor(bgr, gray, COLOR_BGR2GRAY);
-    cvtColor(bgr, hsv, COLOR_BGR2HSV);
-    imwrite("../images/gray.jpg", gray);
-    imwrite("../images/hsv.jpg", hsv);
-
-
-    // GET BLUE BALL
-    Mat mask;
-    getMaskFromHue(hsv, mask, 100, 110);
-    imwrite("../images/mask.jpg", mask);
-    return 0;
-}
-
 void getImageLayers(Mat &src, Mat *dst)
 {
     for (int bit = 0; bit < 8; bit++)
@@ -56,56 +33,109 @@ void getMaskFromHue(Mat &src, Mat &mask, int begin, int end)
     }
 }
 
+void applyMaskBGR(Mat &src, Mat &dst, Mat &mask, bool remove = false, bool light = false)
+{
+    int reference = remove ? 255 : 0;
+    dst = src.clone();
+    for (int i = 0; i < dst.rows; i++)
+    {
+        for (int j = 0; j < dst.cols; j++)
+        {
+            Vec3b pixel = src.at<Vec3b>(i, j);
+            if (mask.at<uchar>(i, j) == reference)
+            {
+                for (int k = 0; k < 3; k++)
+                    dst.at<Vec3b>(i, j)[k] = light * 255;
+            }
+        }
+    }
+}
+int main()
+{
+    // SETTING VARIABLES
+    char filename[100];
+    std::string labels;
+    Mat layers[8], channels[3];
+    Mat gray, bgr, hsv, mask;
+
+    // RECEIVING IMAGE
+    bgr = imread("../images/ball.jpg", IMREAD_COLOR);
+    // GaussianBlur(bgr, bgr, Size(3, 3), 0, 0, BORDER_DEFAULT);
+
+    // GET GRAY AND HSV
+    cvtColor(bgr, gray, COLOR_BGR2GRAY);
+    cvtColor(bgr, hsv, COLOR_BGR2HSV);
+
+    // CREATE ERODE AND DILATE ELEMENTS
+    Mat rec = getStructuringElement(0, Size(3, 3));
+    Mat cross = getStructuringElement(1, Size(3, 3));
+    Mat ellipse = getStructuringElement(1, Size(3, 3));
+
+    // GET BLUE BALL
+    getMaskFromHue(hsv, mask, 100, 108);
+    imwrite("../images/ball1/mask.jpg", mask);
+    int F_ERODE = 1, F_DILATE = 4, S_ERODE = 3;
+    for (int i = 0; i < F_ERODE; i++)
+        erode(mask, mask, rec);
+    for (int i = 0; i < F_DILATE; i++)
+        dilate(mask, mask, rec);
+    for (int i = 0; i < S_ERODE; i++)
+        erode(mask, mask, rec);
+    applyMaskBGR(bgr, bgr, mask, false, true);
+    imwrite("../images/ball1/ball.jpg", bgr);
+    applyMaskBGR(bgr, bgr, mask, true, true);
+    imwrite("../images/ball2/ball2.jpg", bgr);
+    return 0;
+}
+
 // // GET BIT LAYERS:
-// Mat layers[8];
 // getImageLayers(gray, layers);
-// for(int i = 0; i < 8; i++){
-//     sprintf(filename, "../images/layers/%d.jpg",i);
+// for (int i = 0; i < 8; i++)
+// {
+//     sprintf(filename, "../images/layers/%d.jpg", i);
 //     imwrite(filename, layers[i]);
 // }
 
 // // GET BGR CHANNELS
-// Mat channels[3];
-// char labels[] = "BGR";
+// labels = "BGR";
 // split(bgr, channels);
-// for(int i = 0; i < 3; i++){
-//     sprintf(filename, "../images/channels/bgr/%c.jpg",labels[i]);
+// for (int i = 0; i < 3; i++)
+// {
+//     sprintf(filename, "../images/channels/bgr/%c.jpg", labels[i]);
 //     imwrite(filename, channels[i]);
 // }
 
 // // GET HSV CHANNELS
-// Mat channels[3];
-// char labels[] = "HSV";
+// labels = "HSV";
 // split(hsv, channels);
-// for(int i = 0; i < 3; i++){
-//     sprintf(filename, "../images/channels/hsv/%c.jpg",labels[i]);
+// for (int i = 0; i < 3; i++)
+// {
+//     sprintf(filename, "../images/channels/hsv/%c.jpg", labels[i]);
 //     imwrite(filename, channels[i]);
 // }
 
 // // GET BGR CHANNELS LAYERS
-// Mat layers[8];
-// Mat channels[3];
-// char labels[] = "BGR";
+// labels = "BGR";
 // split(bgr, channels);
-// for(int i = 0; i < 3; i++){
+// for (int i = 0; i < 3; i++)
+// {
 //     getImageLayers(channels[i], layers);
 //     for (int j = 0; j < 8; j++)
 //     {
-//         sprintf(filename, "../images/channels/bgr/%c/%d.jpg",labels[i],j);
+//         sprintf(filename, "../images/channels/bgr/%c/%d.jpg", labels[i], j);
 //         imwrite(filename, layers[j]);
 //     }
 // }
 
 // // GET HSV CHANNELS LAYERS
-// Mat layers[8];
-// Mat channels[3];
-// char labels[] = "HSV";
+// labels = "HSV";
 // split(hsv, channels);
-// for(int i = 0; i < 3; i++){
+// for (int i = 0; i < 3; i++)
+// {
 //     getImageLayers(channels[i], layers);
 //     for (int j = 0; j < 8; j++)
 //     {
-//         sprintf(filename, "../images/channels/hsv/%c/%d.jpg",labels[i],j);
+//         sprintf(filename, "../images/channels/hsv/%c/%d.jpg", labels[i], j);
 //         imwrite(filename, layers[j]);
 //     }
 // }
